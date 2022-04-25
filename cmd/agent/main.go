84666cdf2ec30070277e9b5a026215348826e18e
S. Guliaev <devsagul@gmail.com>
2022-04-26 00:42:45 +0300

    draft first increment

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,46 @@
 package main
 
-func main() {}
+import (
+       "time"
+       "fmt"
+       "logogger/internal/poller"
+       "logogger/internal/reporter"
+)
+
+const (
+      pollInterval = 2 * time.Second
+      reportInterval = 10 * time.Second
+      reportHost = "127.0.0.1:8000"
+)
+
+func main() {
+  pollTicker := time.NewTicker(pollInterval)
+  reportTicker := time.NewTicker(reportInterval)
+  channel := make(chan poller.Metrics)
+  responsesChannel := make(chan reporter.ServerResponse)
+
+  go func() {
+     p := poller.Poller(0)
+     for {
+      	  <-pollTicker.C
+      	  channel <-p()
+      }
+  }()
+
+  var m poller.Metrics
+
+  go func() {
+      for {
+          select {
+	      case metrics := <-channel:
+	          m = metrics
+	      case <-reportTicker.C:
+	          reporter.ReportMetrics(m, reportHost, responsesChannel)
+	      case r := <-responsesChannel:
+	      	   fmt.Println(r)
+	  }
+      }
+  }()
+
+  time.Sleep(65 * time.Second)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module logogger
+
+go 1.18
