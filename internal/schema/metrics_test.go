c94b98578f5af402deca3ebfc6ced7f6345288fc
S. Guliaev <devsagul@gmail.com>
2022-05-02 15:54:51 +0300

    refactor schema tests

--- internal/schema/metrics_test.go
+++ internal/schema/metrics_test.go
@@ -6,17 +6,16 @@ import (
 	"testing"
 )
 
-var serializationTests = []struct {
-	in  Metrics
-	out string
-}{
-	{NewCounterRequest("counterID"), `{"id": "counterID", "type": "counter"}`},
-	{NewGaugeRequest("gaugeID"), `{"id": "gaugeID", "type": "gauge"}`},
-	{NewCounter("counterID", 42), `{"id": "counterID", "type": "counter", "delta": 42}`},
-	{NewGauge("gaugeID", 13.37), `{"id": "gaugeID", "type": "gauge", "value": 13.37}`},
-}
-
 func TestMetrics_CreateAndSerialize(t *testing.T) {
+	var serializationTests = []struct {
+		in  Metrics
+		out string
+	}{
+		{NewCounterRequest("counterID"), `{"id": "counterID", "type": "counter"}`},
+		{NewGaugeRequest("gaugeID"), `{"id": "gaugeID", "type": "gauge"}`},
+		{NewCounter("counterID", 42), `{"id": "counterID", "type": "counter", "delta": 42}`},
+		{NewGauge("gaugeID", 13.37), `{"id": "gaugeID", "type": "gauge", "value": 13.37}`},
+	}
 	for _, data := range serializationTests {
 		serialized, err := json.Marshal(data.in)
 		assert.Nil(t, err)
